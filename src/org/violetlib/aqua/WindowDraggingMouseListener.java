cd3f78af8f7a3b91369373f6734a8c4b4155790f
Alan Snyder <ranchlens@cbfiddle.com>
2016-07-15 12:18:18 -0700

    Fix copyright notice

--- src/org/violetlib/aqua/WindowDraggingMouseListener.java
+++ src/org/violetlib/aqua/WindowDraggingMouseListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015=2016 Alan Snyder.
+ * Copyright (c) 2015-2016 Alan Snyder.
  * All rights reserved.
  *
  * You may not use, copy or modify this file, except in compliance with the license agreement. For details see
