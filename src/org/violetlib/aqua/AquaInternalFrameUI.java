873d38412da7ab46dc4c05fbf60caca0eb9fed41
Alan Snyder <ranchlens@cbfiddle.com>
2018-10-03 13:04:35 -0700

    inhibit iconification of simulated frame while maximized

--- src/org/violetlib/aqua/AquaInternalFrameUI.java
+++ src/org/violetlib/aqua/AquaInternalFrameUI.java
@@ -734,7 +734,7 @@ public class AquaInternalFrameUI extends BasicInternalFrameUI implements SwingCo
                 configureFrameType();
             } else if ("windowModified".equals(name) || "Window.documentModified".equals(name)) {
                 // repaint title bar
-                setDocumentEdited(((Boolean)e.getNewValue()).booleanValue());
+                setDocumentEdited((Boolean) e.getNewValue());
                 frame.repaint(0, 0, frame.getWidth(), frame.getBorder().getBorderInsets(frame).top);
             } else if ("resizable".equals(name) || "state".equals(name) || "iconable".equals(name) || "maximizable".equals(name) || "closable".equals(name)) {
                 if ("resizable".equals(name)) {
@@ -742,6 +742,8 @@ public class AquaInternalFrameUI extends BasicInternalFrameUI implements SwingCo
                 }
                 frame.repaint();
             } else if ("title".equals(name)) {
+                frame.repaint();
+            } else if (JInternalFrame.IS_MAXIMUM_PROPERTY.equals(name)) {
                 frame.repaint();
             } else if ("componentOrientation".equals(name)) {
                 frame.revalidate();
@@ -752,9 +754,8 @@ public class AquaInternalFrameUI extends BasicInternalFrameUI implements SwingCo
             } else if (JInternalFrame.MENU_BAR_PROPERTY.equals(name)) {
                 menuBarInstalled();
             }
-
         }
-    } // end class PaletteListener
+    }
 
     private void repaintButtons() {
         if (fAquaBorder != null) {
