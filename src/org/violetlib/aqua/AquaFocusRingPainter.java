dd9bb7eac5203f9632c546820bdcfc32be0e725e
Alan Snyder <ranchlens@cbfiddle.com>
2020-08-17 15:43:42 -0700

    Bug fix: NPE in focus ring painter (backport).

--- src/org/violetlib/aqua/AquaFocusRingPainter.java
+++ src/org/violetlib/aqua/AquaFocusRingPainter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2019 Alan Snyder.
+ * Copyright (c) 2015-2020 Alan Snyder.
  * All rights reserved.
  *
  * You may not use, copy or modify this file, except in compliance with the license agreement. For details see
@@ -66,6 +66,8 @@ public class AquaFocusRingPainter {
                         currentFocusRingOwner = sp;
                         currentFocusRingProvider = defaultFocusRingProvider;
                     }
+                } else {
+                    currentFocusRingProvider = null;
                 }
             }
         }
