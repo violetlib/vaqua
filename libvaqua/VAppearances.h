ae351bd77a2e390b6c69c3c3712f97729bdb3a40
Karl von Randow <karl@xk72.com>
2020-09-26 17:23:57 +1200

    libvaqua: added VAppearances.h from VAppearances repo
    As it is required to compile.

---
+++ libvaqua/VAppearances.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2018 Alan Snyder.
+ * All rights reserved.
+ *
+ * You may not use, copy or modify this file, except in compliance with the license agreement. For details see
+ * accompanying license terms.
+ */
+
+#ifndef __VAPPEARANCES__
+#define __VAPPEARANCES__
+
+#import <Cocoa/Cocoa.h>
+
+void VAppearances_updateAppearance(NSAppearance *appearance);
+
+#endif /* __VAPPEARANCES__ */
