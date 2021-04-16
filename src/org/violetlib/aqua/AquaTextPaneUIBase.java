9a3a81ccef9c8203b2d302fed78389618df4716f
Alan Snyder <ranchlens@cbfiddle.com>
2021-04-15 20:20:01 -0700

    Avoid updating the scroll pane border of a text component that is not displayable.

--- src/org/violetlib/aqua/AquaTextPaneUIBase.java
+++ src/org/violetlib/aqua/AquaTextPaneUIBase.java
@@ -194,7 +194,9 @@ public class AquaTextPaneUIBase extends AquaTextComponentUIBase {
         @Override
         public void hierarchyChanged(@NotNull HierarchyEvent e) {
             if ((e.getChangeFlags() & HierarchyEvent.PARENT_CHANGED) != 0) {
-                updateBorderOwner();
+                if (editor != null && editor.isDisplayable()) {
+                    updateBorderOwner();
+                }
             }
         }
     }
