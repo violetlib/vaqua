60f9abfe9c4fda51790957a41db641ff4defcea5
Alan Snyder <ranchlens@cbfiddle.com>
2024-02-26 16:44:39 -0800

    Add header comment

--- native/jnu_support.m
+++ native/jnu_support.m
@@ -1,4 +1,7 @@
-// Based on part on OpenJDK
+/*
+ Based on part on OpenJDK
+*/
+
 /*
  * Copyright (c) 2011, 2013, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
