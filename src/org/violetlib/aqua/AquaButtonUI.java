d904f68959ce60ae05d3b5b829e846d00e30cdd9
Alan Snyder <ranchlens@cbfiddle.com>
2024-09-23 18:42:06 -0700

    Port fix for JDK-4368790.

--- src/org/violetlib/aqua/AquaButtonUI.java
+++ src/org/violetlib/aqua/AquaButtonUI.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2023 Alan Snyder.
+ * Copyright (c) 2015-2024 Alan Snyder.
  * All rights reserved.
  *
  * You may not use, copy or modify this file, except in compliance with the license agreement. For details see
@@ -885,6 +885,7 @@ public class AquaButtonUI extends BasicButtonUI
             // If focusLost arrives while the button has been left-clicked this would disarm the button,
             // causing actionPerformed not to fire on mouse release!
             //b.getModel().setArmed(false);
+            b.getModel().setPressed(false);
             ((Component)e.getSource()).repaint();
         }
 
